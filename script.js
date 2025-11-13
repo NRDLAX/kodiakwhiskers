b92390186ce833fa2efc89d45fdf79947222ba93
Aldrin Maqyl Bin Adira <aadi0015@student.monash.edu>
2025-11-13 19:00:28 +0800

    Added background video loop element. Animated hover effect of full logo at the center of the page. Top left compact logo leads to instagram page. Hamburger menu has bubble drip effect. Other pages have been set up with no content yet. Page tab icon has been added to use the stamp mark.

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger-menu');
+    const menuContainer = document.querySelector('.menu-container');
+
+    hamburger.addEventListener('click', () => {
+        menuContainer.classList.toggle('is-open');
+    });
+});
